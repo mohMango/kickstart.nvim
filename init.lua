f089cbf0376deb4dcd0bb22ba4bf185439c49aef
Mohamed Ibraheem <moh.mango12@gmail.com>
2023-10-11 12:13:06 +0300

    catppuccin theme and lsp_zero maps

--- init.lua
+++ init.lua
@@ -156,7 +156,26 @@ require('lazy').setup({
       vim.g.gruvbox_material_ui_contrast = 'high'
       vim.g.gruvbox_material_diagnostic_line_highlight = 1
       vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
-      vim.cmd.colorscheme 'gruvbox-material'
+      -- vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup({
+        flavour = 'macchiato',          -- latte, frappe, macchiato, mocha
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+        term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+        no_italic = false,              -- Force no italic
+        no_bold = false,                -- Force no bold
+        no_underline = false,           -- Force no underline
+      })
+      -- setup must be called before loading
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
@@ -167,7 +186,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'gruvbox',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
@@ -230,6 +249,21 @@ require('lazy').setup({
     config = function(_, opts) require 'lsp_signature'.setup(opts) end
   },
   { "mbbill/undotree" },
+  {
+    "windwp/nvim-autopairs",
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require("nvim-autopairs").setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+  }
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -459,13 +493,6 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>lk', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', '<leader>lj', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
-
 -- Setup neovim lua configuration
 require('neodev').setup()
 
@@ -474,13 +501,29 @@ require('neodev').setup()
 local lsp_zero = require 'lsp-zero'
 
 lsp_zero.extend_lspconfig()
-lsp_zero.on_attach(function(_, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+lsp_zero.on_attach(function(_, _)
   vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { desc = '[l]sp code [a]ction' })
   vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = '[l]sp [r]enames' })
-  vim.keymap.set('n', '<leader>lf', ':LspZeroFormat<cr>', { desc = '[l]sp[f]ormat Buffers' })
+  vim.keymap.set({ 'n', 'x' }, '<leader>lf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
+    { desc = '[l]sp [f]ormat Buffer' })
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover' })
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = '[g]o [d]efinition' })
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '[g]o [D]eclaration' })
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = '[g]o [i]mplementation' })
+  vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, { desc = 'Type Definition' })
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = '[g]o [r]eferences' })
+  vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { desc = '[g]o [s]ignature' })
+  -- Diagnostic keymaps
+  vim.keymap.set('n', '<leader>lk', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+  vim.keymap.set('n', '<leader>lj', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+  vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+  vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+  if vim.lsp.buf.range_code_action then
+    vim.keymap.set('x', '<leader>la', vim.lsp.buf.range_code_action, { desc = '[l]sp code [a]ction' })
+  else
+    vim.keymap.set('x', '<leader>la', vim.lsp.buf.code_action, { desc = '[l]sp code [a]ction' })
+  end
 end)
 
 require 'mason-lspconfig'.setup({
@@ -489,15 +532,39 @@ require 'mason-lspconfig'.setup({
   }
 })
 
+local cmp_action = require 'lsp-zero'.cmp_action()
 local cmp = require 'cmp'
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      select = true,
+    },
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  }),
+  formatting = {
+    -- changing the order of fields so the icon is the first
+    fields = { 'menu', 'abbr', 'kind' },
 
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-  })
+    -- here is where the change happens
+    format = function(entry, item)
+      local menu_icon = {
+        nvim_lsp = 'λ',
+        luasnip = '⋗',
+        buffer = 'Ω',
+        path = '🖫',
+        nvim_lua = 'Π',
+      }
+
+      item.menu = menu_icon[entry.source.name]
+      return item
+    end,
+  },
 })
 
 -- [[ NETRW ]]
