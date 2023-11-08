b3e40ea408f2ff3b22da187d0d5cd6b85171e77d
Mohamed Ibraheem <moh.mango12@gmail.com>
2023-11-08 20:48:36 +0200

    add formatter

--- init.lua
+++ init.lua
@@ -270,6 +270,35 @@ require('lazy').setup({
     end,
   },
   { 'Ciel-MC/rust-tools.nvim' },
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local conform = require 'conform'
+      conform.setup {
+        formatters_by_ft = {
+          javascript = { { 'prettierd', 'prettier' } },
+          typescript = { { 'prettierd', 'prettier' } },
+          javascriptreact = { { 'prettierd', 'prettier' } },
+          typescriptreact = { { 'prettierd', 'prettier' } },
+          svelte = { { 'prettierd', 'prettier' } },
+          css = { { 'prettierd', 'prettier' } },
+          html = { { 'prettierd', 'prettier' } },
+          json = { { 'prettierd', 'prettier' } },
+          lua = { 'stylua' },
+          cpp = { 'clang_format' },
+        },
+      }
+
+      vim.keymap.set({ 'n', 'x' }, '<leader>lf', function()
+        conform.format {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        }
+      end, { desc = '[l]sp [f]ormat Buffer' })
+    end,
+  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
