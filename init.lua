fb9025519c78aff0fecf340bfad1451bd19df265
Mohamed Ibraheem <moh.mango12@gmail.com>
2023-11-09 02:27:39 +0200

    fix gitsigns error

--- init.lua
+++ init.lua
@@ -131,6 +131,7 @@ require('lazy').setup({
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
